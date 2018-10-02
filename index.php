626b719cc2622cd88c97026f65f8150c3511c1ba
Ruben Kuilder <rubenkuilder@gmail.com>
2018-10-02 11:14:57 +0200

    Added whole website structure and base files

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="nl">
+	<head>
+		<title>
+			INF1B_Project
+		</title>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width">
+		
+		<!-- CSS Files -->
+		<link rel="stylesheet" type="text/css" href="../assets/stylesheets/main.css">
+	</head>
+	<body>
+		<!-- PHP language redirect script -->
+	</body>
+</html>
---
+++ readme.txt
@@ -0,0 +1,11 @@
+Project website INF1B.
+
+Johan Rikse
+Sietze koonstra
+Tim Gels
+Bram Gerrits
+Koen Somsen
+Yoran Grobbe
+Jarno Waijerink
+Daniel Roosken
+Ruben Kuilder
