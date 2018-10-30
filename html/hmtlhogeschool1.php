2556b9a7c1edb9f94a2231b8010786deb95e91f6
DESKTOP-CAOVDPD\Alexander <bramgggeee@gmail.com>
2018-10-30 15:26:42 +0100

    Eco responsive (nog geen content!)

--- html/hmtlhogeschool1.php
+++ html/hmtlhogeschool1.php
@@ -7,7 +7,6 @@
 
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 		<link href="https://fonts.googleapis.com/css?family=Raleway:300,400,700,900" rel="stylesheet">
-                 <!--<link rel="stylesheet" type="text/css" href="Main.css">-->
 		
 		<title> Hogeschool Hoogeveen</title>
 	</head>
@@ -30,21 +29,31 @@
 			
 			<div class=streepje></div>
 			
-			<ul id="desktopMenu">
-				<li> Agenda</li>
-				<li> Inschrijven</li>
-				<li> Roosters</li>
-				<li> Media</li>	
-				<li> Opleidingen</li>				
+			<ul class="desktopMenu">
+				<li><a href="#">Agenda</a></li>
+				<li><a href="#">Inschrijven</a></a></li>
+				<li><a href="#">Media</a></a></li>	
+				<li class="dropdown">
+					<a href="#" class="dropbtn">Opleidingen</a>
+					<div class="dropdown-content">
+						<a href="#">Opleiding 1</a>
+						<a href="#">Opleiding 2</a>
+					</div>
+				</li>			
 			</ul>
 			
 			<div id="menuBtn">☰</div>
-			<ul id="mobileMenu">
-				<li> Agenda</li>
-				<li> Inschrijven</li>
-				<li> Roosters</li>
-				<li> Media</li>	
-				<li> Opleidingen</li>				
+			<ul class="mobileMenu">
+				<li>Agenda</li>
+				<li>Inschrijven</li>
+				<li>Media</li>
+				<li class="dropdown">
+					<a href="#" class="dropbtn">Opleidingen</a>
+					<div class="dropdown-content">
+						<a href="#">Opleiding 1</a>
+						<a href="#">Opleiding 2</a>
+					</div>
+				</li>
 			</ul>				
 		</div>
 		<div id=content>
@@ -52,21 +61,22 @@
 			</div>
 			
 			<div id='achtergrondcontent'>
-				<div id='backgroundimage'> </div>
+				<div id='backgroundimageECO'> </div>
 			</div>
 							
 			<div id='textbox'>
 				<h1> Bedrijfseconomie & Logistiek</h1>
-				<p>wij geven economie
+				<p> Wij geven economie 
 				
 				</p>
-				<a href='#' class=moreknop> More </a>
+			
+			<div class="meerknop">
+				<a href='#meerinformatie'> More </a>
+			</div>
+			
+			</div>
 
-			
-			</div>
-			<div class='bollen'> 
-			</div>
-			
+			<div class="clearFix"></div>
 		
 		</div>
                         <div class="informatiecontent">
@@ -79,12 +89,12 @@
                
                 <script> define('SCRIPT_DEBUG', true);</script>
         </section>
-                                    
-                                </div>
-        </div>
-                            <div class="bovensteBalk">
-                                
-                            </div>
+
+                </div>
+            </div>
+                            <div class="bovensteBalk"></div>
+        <div id="ecocontentbox">       
+            
                             
             <div class="middelsteBlok">
                 <h2> Bedrijfseconomie</h2>
@@ -96,9 +106,8 @@
                
                 </p>
             </div>
-            <div class="rechterBlok">
+            <div class="middelsteBlok">
                 <h2> Logistiek</h2>  
-                                <h2> Bedrijfseconomie</h2>
                 <p> 
                     Als manager in de logistiek heb je afwisselend en verantwoordelijk werk. 
                     Met jouw vakkennis zorg je ervoor dat belangrijke bedrijfsprocessen zoals opslag, vervoer, planning en goederenstroom probleemloos verlopen of ben je verantwoordelijk voor het reilen en zeilen van een productieafdeling.
@@ -106,7 +115,7 @@
                
                 </p>
             </div>
-                            
+                    
             <div class="rssFeedBox">
                 
     <?php 
@@ -132,50 +141,54 @@
                 
     ?>
             </div>
+            
 
-
+        </div>  
 <!-- end sw-rss-feed code -->
 
 
-        <div id="IframeWrapper" style="position: relative;">
+<!--        <div id="IframeWrapper" style="position: relative;">
    
         
     <div id="iframeBlocker" style="position:absolute; top: 0; left: 0; width:95%; height:95%;background-color:aliceblue;opacity:0.1;"></div>
 </div>
+
+-->
+
                             <div class="clearFix"></div>
                         </div>
-
+                
 		
-		<div id=footer>
-			<div id=footercontent>
-			
-				<div class=Links> 
-					<p>Links</p>
-					<ul>
-						<li>Opleidingen </li>
-						<li>Media </li>
-						<li>Roosters </li>
-						<li>Inschrijven </li>
-						<li>Agenda </li>
-					</ul>
-				</div>
-				
-				<div class=Links> 
-					<p>Contact</p>
-					<ul>
-						<li>Contact </li>
-						<li>Informatie </li>
-					</ul>
-				</div>
-				<div id=gegevens>
-				    <p> Adres: Hogeschool Hoogeveen, Hoogestraat 99</p>
-					<p> 06 25-73 21-34</p>
-					<p> help@hogeschoolhoogeveen.nl</p>
-				</div>	
-				
-			</div>
+    <div id=footer>
+            <div id=footercontent>
 
-		</div>
+                    <div class=Links> 
+                            <p>Links</p>
+                            <ul>
+                                    <li>Opleidingen </li>
+                                    <li>Media </li>
+                                    <li>Roosters </li>
+                                    <li>Inschrijven </li>
+                                    <li>Agenda </li>
+                            </ul>
+                    </div>
+
+                    <div class=Links> 
+                            <p>Contact</p>
+                            <ul>
+                                    <li>Contact </li>
+                                    <li>Informatie </li>
+                            </ul>
+                    </div>
+                    <div id=gegevens>
+                        <p> Adres: Hogeschool Hoogeveen, Hoogestraat 99</p>
+                            <p> 06 25-73 21-34</p>
+                            <p> help@hogeschoolhoogeveen.nl</p>
+                    </div>	
+
+            </div>
+
+    </div>
 			<div id=backtotop>
 				<a href='#top'> <p> Back to top</p> </a>
 			</div>
